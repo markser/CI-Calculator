a9a26165863004e29e1371f1bc17199140fbd073
markser <serm@oregonstate.edu>
2021-05-28 20:10:45 -0700

    fix linting errors for the test files

--- test_calculator.py
+++ test_calculator.py
@@ -7,8 +7,7 @@ import calculator
 class TestCalculatorApp:
 
     def test_add(self):
-        assert 5 == calculator.add(3,2)
-
+        assert 5 == calculator.add(3, 2)
 
     def test_subtract(self):
-        assert 1 == calculator.sub(3,2)
+        assert 1 == calculator.sub(3, 2)
