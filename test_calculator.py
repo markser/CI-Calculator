b8fe135b84658c408f12b0ffa1e080e009a3ef36
markser <serm@oregonstate.edu>
2021-05-28 20:16:42 -0700

    added more test cases for calc

--- test_calculator.py
+++ test_calculator.py
@@ -8,12 +8,16 @@ class TestCalculatorApp:
 
     def test_add(self):
         assert 5 == calculator.add(3, 2)
+        assert 10000 != calculator.add(3, 2)
 
     def test_subtract(self):
         assert 1 == calculator.sub(3, 2)
+        assert 10000 != calculator.sub(3, 2)
 
     def test_multiply(self):
         assert 2 == calculator.mul(1, 2)
+        assert 10000 != calculator.mul(3, 2)
 
     def test_divide(self):
         assert 1 == calculator.div(5, 5)
+        assert 10000 != calculator.div(3, 2)
